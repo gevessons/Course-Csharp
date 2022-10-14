35aaa26d5140bcc2307a48946457597e8625e83d
Gevesson Silva <gevesson_silva@hotmail.com>
2022-10-14 16:23:14 -0300

    Entrada de dados 2

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,32 +1,33 @@
 ﻿using System;
+using System.Globalization;
+
 namespace Course {
     public static class Program {
         public static void Main(string[] args) {
 
-            //Entrada de dados
+            //Entrada de dados 2
 
-            string frase = Console.ReadLine();
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            string z = Console.ReadLine();
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            
             string[] v = Console.ReadLine().Split(' ');
-            string a = v[0];
-            string b = v[1];
-            string c = v[2];
+
+            string nome = v[0];
+            char sexo  = char.Parse(v[1]);
+            int idade = int.Parse(v[2]);
+            double altura = double.Parse(v[3], CultureInfo.InvariantCulture);
 
 
-            Console.WriteLine("Você digitou: ");
-            Console.WriteLine(frase);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
+            Console.WriteLine("n1: " + n1);
+            Console.WriteLine("ch: " + ch);
+            Console.WriteLine("n2: " + n2);
 
+            Console.WriteLine("nome: " + nome);
+            Console.WriteLine("sexo: " + sexo);
+            Console.WriteLine("idade: " + idade);
+            Console.WriteLine("altura: " + altura.ToString("F2", CultureInfo.InvariantCulture));
 
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
 
 
         }
