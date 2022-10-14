ee7112202c1e9ce99ef81dcf701d70d0474e6a04
Gevesson Silva <gevesson_silva@hotmail.com>
2022-10-14 18:01:15 -0300

    Entrada de dados 2

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    public static class Program {
+        public static void Main(string[] args) {
+            /*
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+            
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quarto = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);*/
+
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            string[] vet = Console.ReadLine().Split(' ');
+
+
+            /*Console.WriteLine(nome);
+            Console.WriteLine(quarto);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            */
+            Console.WriteLine(vet[0]);
+            Console.WriteLine(int.Parse(vet[1]));
+            Console.WriteLine(double.Parse(vet[2]).ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
