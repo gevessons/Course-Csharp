85db44816e6725ea2279328ce91aad8488be017c
Gevesson Silva <gevesson_silva@hotmail.com>
2022-10-17 15:08:24 -0300

    Entrada de dados 2 - conversão para Double

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -4,7 +4,7 @@ using System.Globalization;
 namespace Course {
     public static class Program {
         public static void Main(string[] args) {
-            /*
+            
             Console.WriteLine("Entre com seu nome completo:");
             string nome = Console.ReadLine();
             
@@ -12,19 +12,24 @@ namespace Course {
             int quarto = int.Parse(Console.ReadLine());
             
             Console.WriteLine("Entre com o preço de um produto:");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);*/
-
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           
+           
             Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
             string[] vet = Console.ReadLine().Split(' ');
 
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            
 
-            /*Console.WriteLine(nome);
+
+            Console.WriteLine(nome);
             Console.WriteLine(quarto);
             Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
-            */
+            
+            
             Console.WriteLine(vet[0]);
-            Console.WriteLine(int.Parse(vet[1]));
-            Console.WriteLine(double.Parse(vet[2]).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(int.Parse(vet[1]));            
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
 
 
         }
